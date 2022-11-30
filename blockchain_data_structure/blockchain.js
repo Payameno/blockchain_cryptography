bb22ec5d44ee4736f18467594b340d2097e5d622
Payam Beigi <beigi.payam@gmail.com>
2022-11-29 18:41:57 -0800

    linking blocks in the blockchain

--- blockchain_data_structure/blockchain.js
+++ blockchain_data_structure/blockchain.js
@@ -9,6 +9,9 @@ class Blockchain {
 
   //Function to add a new block to the blockchain
     addBlock(newBlock) {
+        //Adding previous block Hash to the next block
+        block.previousHash = this.chain[this.chain.length - 1].toHash();
+        //pushing block the the blockchain array
         this.chain.push(newBlock);
     }
 }
