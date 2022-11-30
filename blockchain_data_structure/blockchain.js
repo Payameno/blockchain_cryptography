3b6626e2d812aa8e281059e8a72f58d048950cca
Payam Beigi <beigi.payam@gmail.com>
2022-11-29 19:25:32 -0800

    Validate every block for the hash to be equal to the prev block's

--- blockchain_data_structure/blockchain.js
+++ blockchain_data_structure/blockchain.js
@@ -14,4 +14,19 @@ class Blockchain {
         //pushing block the the blockchain array
         this.chain.push(newBlock);
     }
+
+    isValid() {
+
+      for (let i = this.chain.length -1; i > 0; i--) {
+          const block = this.chain[i]
+          const prev = this.chain[i-1]
+
+          if (block.previousHash.toString() !== prev.toHash().toString()) {
+              return false;
+          }
+      }
+              return true;
+
+  }
+  
 }
